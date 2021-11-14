0906f4185320dc5b5792725873db6267333c56bb
Bryan Marshall <professor@bryanmarshall.com>
2021-11-14 06:17:27 -0500

    changes

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Moto</h1>'
+    return '<h1>DIY Money</h1>'
 
 
 @app.route('/<name>')
 def firstname(name):
-    return f'<h1>Hello Moto {name} the big sec Georgia won last nights game hard </h1>'
+    return f'<h1>Hello DIYer Money Man - {name}. You can be great with your money all on your own.</h1>'
 
 
 if __name__ == '__main__':
